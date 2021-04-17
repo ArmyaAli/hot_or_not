b2c2058b13c5139c7dcdeb469d7d467afb588ff6
ArmyaAli <ali.umar.work@gmail.com>
2021-04-16 22:10:03 -0400

    update db code

---
+++ Server/src/Data_Layer/db.ts
@@ -0,0 +1,26 @@
+import Formidable from 'formidable';
+import path from 'path'
+// let sqlite3 = require('sqlite3').verbose();
+import sqlite3, { verbose } from 'sqlite3'
+
+interface People {
+    name: string
+    gender: string
+    category: string
+    filePath: string
+}
+
+const establishConnection = ()  => {
+    const filePath = path.resolve(__dirname, '../db/test.db')
+
+    let sqlite3 = require('sqlite3').verbose();
+
+    return new sqlite3.Database(filePath)
+}
+
+export const addRow = async (people: People) => {
+    const db = establishConnection();
+    const insert = `INSERT INTO people (name, gender, categories, image) values (${people.name}, ${people.gender}, ${people.category}, ${people.filePath})`
+    db.run(insert)
+    db.close();
+}
