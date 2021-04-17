9f225d9646d2b59b0e94609893c79574533723bb
ArmyaAli <ali.umar.work@gmail.com>
2021-04-16 20:38:55 -0400

    created basic express app

--- Server/src/Server/server.ts
+++ Server/src/Server/server.ts
@@ -1,20 +1,19 @@
-const express = require( "express" );
+import express from 'express'
 const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app
-.get( "/", ( req, res ) => {
-    res.send( "Express Backend" );
-})
-.post("/", (req, res) => {
-    console.log(req)
-    res.send( "" );
-    
-    // databaseQuery();
-})
+
+app.get("/", (req, res) => {
+        res.send("Express Backend");
+    })
+    .post("/", (req, res) => {
+        res.send(req.body);
+
+        // databaseQuery();
+    })
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
