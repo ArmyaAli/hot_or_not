dfe34dcfd3fde41e8a93a9af7e2d88112b8386c4
ArmyaAli <ali.umar.work@gmail.com>
2021-05-21 19:54:25 -0400

    misc

--- Server/src/Data_Layer/db.ts
+++ Server/src/Data_Layer/db.ts
@@ -6,21 +6,19 @@ import sqlite3, { verbose } from 'sqlite3'
 interface People {
     name: string
     gender: string
-    category: string
+    categories: string[]
     filePath: string
 }
 
-const establishConnection = ()  => {
-    const filePath = path.resolve(__dirname, '../db/test.db')
-
+export const establishConnection = ()  => {
+    const filePath = path.resolve(__dirname, '../../db/test.db')
     let sqlite3 = require('sqlite3').verbose();
-
     return new sqlite3.Database(filePath)
 }
 
 export const addRow = async (people: People) => {
     const db = establishConnection();
-    const insert = `INSERT INTO people (name, gender, categories, image) values (${people.name}, ${people.gender}, ${people.category}, ${people.filePath})`
+    const insert = `INSERT INTO people (name, gender, categories, filePath) values ('${people.name}', '${people.gender}', '${people.categories.join(', ')}', '${people.filePath}')`
     db.run(insert)
     db.close();
 }
