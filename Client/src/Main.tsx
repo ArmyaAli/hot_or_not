01c125a818ac2f22f4f412c9e947e76d07e40c91
ArmyaAli <ali.umar.work@gmail.com>
2021-05-28 19:51:16 -0400

    rename App -> Main

--- Client/src/Main.tsx
+++ Client/src/Main.tsx
@@ -1,72 +1,23 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import ReactHtmlParser from 'react-html-parser';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { MenuPage } from "./Pages/MenuPage";
+import { SubmissionPage } from "./Pages/SubmissionPage";
 
-
-const App = () => {
-  const [img, setImg] = useState<string>();
-  
-  const hitEndpoint = async () => {
-    const response = await axios
-      .get("http://localhost:8080")
-      .then(function (response) {
-        console.log(response);
-        setImg(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-
-  useEffect(() => {
-    hitEndpoint();
-  }, []);
-
+const Main = () => {
   return (
     <div>
-      {ReactHtmlParser(img!)}
-      <form
-        action="http://127.0.0.1:8080"
-        method="post"
-        encType="multipart/form-data"
-      >
-        <div>
-          <label>Name: </label>
-          <input type="text" name="name" />
-        </div>
-        <div>
-          <label>Gender: </label>
-          <select name="gender">
-            <option value="M">Male</option>
-            <option value="F">Female</option>
-            <option value="NB">Non-Binary</option>
-          </select>
-        </div>
-        <div>
-          <label>
-            <b>Categories</b>
-          </label>
-          <div>
-            <input type="checkbox" name="cat0" value="Streamers" />
-            <label>Streamer</label>
-            <input type="checkbox" name="cat1" value="Youtubers" />
-            <label>Youtubers</label>
-            <input type="checkbox" name="cat2" value="Porn Stars" />
-            <label>Porn Stars</label>
-            <input type="checkbox" name="cat3" value="Celebrities" />
-            <label>Celebrities</label>
-          </div>
-        </div>
-        <div>
-          <label>Image: </label>
-          <input type="file" name="image" />
-        </div>
-        <div>
-          <input type="Submit" />
-        </div>
-      </form>
+      <Router>
+          <Switch>
+            <Route path="/submission">
+              <SubmissionPage />
+            </Route>
+            <Route path="/">
+              <MenuPage />
+            </Route>
+          </Switch>
+      </Router>
     </div>
   );
 };
 
-export default App;
+export default Main;
