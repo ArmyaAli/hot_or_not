2bf4dea55df99f8362e20adef4d6c0fc00013ed5
ArmyaAli <ali.umar.work@gmail.com>
2021-05-29 17:00:16 -0400

    cleanup

--- Client/src/Main.tsx
+++ Client/src/Main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import { MenuPage } from "./Pages/MenuPage";
 import { SubmissionPage } from "./Pages/SubmissionPage";
 
