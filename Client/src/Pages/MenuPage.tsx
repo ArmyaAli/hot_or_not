eb8765c626df4b70f0e39645267b1264fc539c12
ArmyaAli <ali.umar.work@gmail.com>
2021-05-29 17:15:07 -0400

    template code

--- Client/src/Pages/MenuPage.tsx
+++ Client/src/Pages/MenuPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Footer } from '../Components/Footer/Footer';
+import { MenuGrid } from '../Components/MenuGrid/MenuGrid';
 import { Navbar } from '../Components/Navbar/Navbar'
 
 export const MenuPage = () => {
     return (
         <div>
             <Navbar />
+            <MenuGrid categories={["Streamers", "Porn Stars", "Model"]}/>
             <Footer />
         </div>
         
