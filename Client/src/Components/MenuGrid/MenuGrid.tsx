0cbf7b4c5f314ce338712490ea768552a98de015
ArmyaAli <ali.umar.work@gmail.com>
2021-05-29 17:38:32 -0400

    Menugrid basic behaviour using flexbox

--- Client/src/Components/MenuGrid/MenuGrid.tsx
+++ Client/src/Components/MenuGrid/MenuGrid.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { MenuButton } from "./MenuButton/MenuButton";
+import "./MenuGrid.css"
 
 export interface GridProps {
     categories: string[];
 }
 export const MenuGrid = (props: GridProps) => {
   return (
-    <div>
+    <div className="button-container">
         {props.categories.map((category) => <MenuButton Text={category} /> )}
     </div>
   );
