a8474b2b22d5309c75a2a165a0ee4dc3be79650f
ArmyaAli <ali.umar.work@gmail.com>
2021-05-28 20:13:44 -0400

    updated for reviwe

---
+++ Client/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Navbar.css'
+
+export const Navbar = () => {
+  return(
+    <nav>
+        <div className="nav-wrapper">
+            <div className="home-button-wrapper">
+                <button className="home-button">Home page</button>
+            </div>
+        </div>
+    </nav>
+  )
+};
--- Client/src/Pages/MenuPage.tsx
+++ Client/src/Pages/MenuPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Footer } from '../Components/Footer/Footer';
+import { Navbar } from '../Components/Navbar/Navbar'
 
 export const MenuPage = () => {
     return (
         <div>
-            Hello World
+            <Navbar />
+            <Footer />
         </div>
+        
     )
 }
