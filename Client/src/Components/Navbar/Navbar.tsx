a761d1a737339bb72ac5ad094e2f5677a800e1aa
ArmyaAli <ali.umar.work@gmail.com>
2021-05-29 17:00:27 -0400

    button styling

--- Client/src/Components/Navbar/Navbar.tsx
+++ Client/src/Components/Navbar/Navbar.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import './Navbar.css'
+import "./Navbar.css";
 
 export const Navbar = () => {
-  return(
+  return (
     <nav>
-        <div className="nav-wrapper">
-            <div className="home-button-wrapper">
-                <button className="home-button w-96 bg-gray-50">Home page</button>
-            </div>
+      <div className="nav-wrapper">
+        <div className="back-button-wrapper bg-gray-50 rounded-sm hover:bg-gray-500 cursor-pointer">
+          <button className="back-button h-12 focus:outline-none">Go Back</button>
         </div>
+        <div className="home-button-wrapper bg-gray-50 rounded-sm hover:bg-gray-500 cursor-pointer">
+          <button className="home-button h-12 focus:outline-none">Home Page</button>
+        </div>
+        <div className="add-person-button-wrapper bg-gray-50 rounded-sm hover:bg-gray-500 cursor-pointer">
+          <button className="add-person-button h-12 focus:outline-none">Add Person</button>
+        </div>
+      </div>
     </nav>
-  )
+  );
 };
