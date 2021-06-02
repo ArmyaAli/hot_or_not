b4379bbec036fee77c6458264c32efb1b79e579a
Ringo Chui <ringochewyis@gmail.com>
2021-06-02 15:50:48 -0400

    Created footer with team's personal websites

--- Client/src/Components/Footer/Footer.tsx
+++ Client/src/Components/Footer/Footer.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Footer.css';
 
 export const Footer = () => {
     return (
-        <div>
-            <p>Contact information</p>
-        </div>
+        <footer className="footer-wrapper font-sans">
+            <section className="footer-social">
+                <h2 className="footer-title text-center font-semibold text-2xl">socials</h2>
+                <ul className="footer-social-list">
+                    <li className="footer-social-icons font-semibold"><a href="https://www.aliumar.dev/">A</a></li>
+                    <li className="footer-social-icons font-semibold"><a href="https://github.com/RIngochui">R</a></li>
+                    <li className="footer-social-icons font-semibold"><a href="https://gardio.net/">K</a></li>
+                </ul>
+            </section>
+
+            <section className="footer-legal font-semibold">
+                <ul className="footer-legal-list">
+                    <li>Powered by ARK Team&trade;</li> 
+                    <li>&copy; Copyright <i>Hot or Not Twitch</i>.</li>
+                </ul>g
+            </section>
+        </footer>
     )
 }
