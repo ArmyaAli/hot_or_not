854bc7c5b79aaf6ee7c918d5de66867688f5dedb
ArmyaAli <ali.umar.work@gmail.com>
2021-06-18 18:25:57 -0400

    removed menuPage

--- Client/src/Main.tsx
+++ Client/src/Main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { HomePage } from "./Pages/HomePage";
-import { MenuPage } from "./Pages/MenuPage";
 import { SubmissionPage } from "./Pages/SubmissionPage";
 
 const Main = () => {
@@ -10,9 +9,6 @@ const Main = () => {
         <Switch>
           <Route path="/submission">
             <SubmissionPage />
-          </Route>
-          <Route path="/menu">
-            <MenuPage />
           </Route>
           <Route path="/">
             <HomePage />
