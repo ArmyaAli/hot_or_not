e9b506a7bcefd314ab4abbc1eeaea70a3ee3ce61
ArmyaAli <ali.umar.work@gmail.com>
2021-06-18 18:26:02 -0400

    Added Categories

--- Client/src/Pages/HomePage.tsx
+++ Client/src/Pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Footer } from "../Components/Footer/Footer";
+import { MenuGrid } from "../Components/MenuGrid/MenuGrid";
 import { Navbar } from "../Components/Navbar/Navbar";
 import "./styles/HomePage.css";
 
@@ -11,7 +12,17 @@ export const HomePage = () => {
 
       </div>
       <div className="content-wrapper mid">
-
+        <MenuGrid
+          categories={[
+            "Male Streamers",
+            "Female Streamers",
+            "Male Porn Stars",
+            "Female Porn Stars",
+            "Male Celebs",
+            "Female Celebs",
+            "Other"
+          ]}
+        />
       </div>
       <div className="content-wrapper bot">
 
